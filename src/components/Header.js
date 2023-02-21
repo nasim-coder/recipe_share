016b42bf49500190bb2fa13ff853783404e98704
Nasim Ahmad <khannasimahmad@gmail.com>
2023-02-21 09:48:47 +0530

    created react app

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+      <div>
+          <ul>
+              <li><a href="">login</a></li>
+              <li><a href=""></a>hello</li>
+              <li><a href=""></a>hi</li>
+              <li><a href=""></a>go</li>
+              </ul>
+    </div>
+  )
+}
+
+export default Header
