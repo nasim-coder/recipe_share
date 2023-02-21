340d7bccd861f264e5c024294a31bf46ba22f326
Nasim Ahmad <khannasimahmad@gmail.com>
2023-02-21 14:23:39 +0530

    added nav bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,31 @@
 import React from 'react'
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import logo from '../images/recipe_logo.png';
 
 const Header = () => {
-  return (
-      <div>
-          <ul>
-              <li><a href="">login</a></li>
-              <li><a href=""></a>hello</li>
-              <li><a href=""></a>hi</li>
-              <li><a href=""></a>go</li>
-              </ul>
-    </div>
-  )
+    return (
+        <>
+            <Navbar bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">
+                    <img
+                                src={logo}
+                                width="30"
+                                height="30"
+                                className="d-inline-block align-top"
+                                alt="React Bootstrap logo"
+                            />
+                    </Navbar.Brand>
+                    <Nav className="ml-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">Account</Nav.Link>
+                        <Nav.Link href="#pricing">Login</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+            <img src="" alt="" />
+        </>
+    )
 }
 
 export default Header
