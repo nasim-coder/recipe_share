c7df412a539fa2aefbb65c1fc308cec0f95a4dad
Nasim Ahmad <khannasimahmad@gmail.com>
2023-02-22 16:59:39 +0530

    completed featured design

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -1,11 +1,19 @@
 import React from 'react'
-
+import featureImg from '../images/featured.jpeg'
+import { Button } from 'react-bootstrap'
 const Featured = () => {
   return (
-      <>
-          <img src="" alt="" />
-          <h2>RECIPE OF THE DAY</h2>
-      </>
+    <>
+      <h1>Featured Recipe</h1>
+      <div className="feature-img-container">
+            <img src={featureImg} className="feature-image" alt='feature' />
+            <div className="feature-div">
+                <h1 className='featured-recipe-name'>Mutton Biryani</h1>
+          <p className="m-0">Hyderabadi mutton biryani is a classic dish made by layering rice over slow cooked mutton gravy</p>
+          <Button variant="outline-success float-end">Know More</Button>{''}
+            </div>
+      </div>
+    </>
   )
 }
 
